0bb10f305bd27a9be9b287ab593151e335882f3a
Randel Jiao <randeljiao@gmail.com>
2019-09-19 21:25:26 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
 # Add your code here
+
+class Dog 
+  attr_accessor = :name
+  
+  puppy = []
+  
+  def initialize(puppy_name)
+    @@all = puppy_name
+   
+  end
+  
+  def all
+    @@all = []
+  end
+  
+  def print_all
+    @@all each |x| do
+      x << puppy
+    end
+  
+  def save
+    @@all << puppy
+end
