7617db2e65eb3bebfa305e4dda5a63f944b5ae67
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-19 03:22:58 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
-# Add your code here
+lass Dog 
+  
+  @@all =[]
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all(name)
+    @@all.delete
+  end
+  
+  def self.print_all
+    p @@all
+  end
+end
